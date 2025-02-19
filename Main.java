060c975dfbfc45b238c259b38329921d486731de
Joan1468 <j8810057@gmail.com>
2025-02-18 19:00:04 -0500

    resuelto

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+public class Main{
+    public static void main(String[] args) {
+        System.out.print("Tengo una gata grande ");
+        System.out.print("2 años");
+        System.out.print("como 3 veces al dia");
 
-public class Main {
+    }
 }
